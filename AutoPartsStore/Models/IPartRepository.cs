e70d4ba9dc4d72f8cebe6711385f24c50d408838
Stoycho Kostov <stoichokostov@gmail.com>
2021-07-05 16:54:57 -0500

    adding interfaces

---
+++ AutoPartsStore/Models/IPartRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoPartsStore.Models
+{
+   public interface IPartRepository
+   {
+      IEnumerable<Part> GetAllParts { get; }
+      IEnumerable<Part> GetPartsOnSale { get; }
+      Part GetPartById(int partId);
+   }
+}
