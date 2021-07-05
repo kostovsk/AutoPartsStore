cf0bcafb8c4cc1d9b4dec68caa52a2670fae976d
Stoycho Kostov <stoichokostov@gmail.com>
2021-07-05 16:36:18 -0500

    adding models

---
+++ AutoPartsStore/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoPartsStore.Models
+{
+   public class Category
+   {
+      public int CategoryId { get; set; }
+      public string CategoryName { get; set; }
+      public string CategoryDescription { get; set; }
+      public List<Part> Parts { get; set; }
+
+   }
+}
