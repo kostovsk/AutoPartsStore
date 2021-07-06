d3e96bb1a0333e82881ec841cb40cbacbbafbd17
Stoycho Kostov <stoichokostov@gmail.com>
2021-07-05 20:27:26 -0500

    adding part and category repositories

---
+++ AutoPartsStore/Models/CategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoPartsStore.Models
+{
+   public class CategoryRepository : ICategoryRepository
+   {
+      public IEnumerable<Category> GetAllCategories => new List<Category> { 
+         new Category{CategoryId=1, CategoryName="Brakes", CategoryDescription="Parts for the braking system of your vehicle"},
+         new Category{CategoryId=2, CategoryName="Engine", CategoryDescription="Parts for the engine of your vehicle"},
+         new Category{CategoryId=3, CategoryName="Suspension", CategoryDescription="Parts for the suspension of your vehicle"}
+      };
+   }
+}
---
+++ AutoPartsStore/Models/PartRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoPartsStore.Models
+{
+   public class PartRepository : IPartRepository
+   {
+      private readonly ICategoryRepository _categoryRepository = new CategoryRepository();
+      public IEnumerable<Part> GetAllParts => new List<Part> { 
+         new Part {PartId=1, Name="POWER STOP JBR962EVC Evolution Geomet Coated", Price=18.00M, Description="Power Stop's Evolution Coated rotors are direct bolt-on replacements for your daily driven vehicle. These rotors are fully coated, including inside the vanes, using Genuine GEOMET Coating for long lasting protection against rust and corrosion. The aluminum silver color of the coating looks great with today's open alloy wheels. To ensure a trouble-free installation, these rotors are OE weight and vane configuration to guarantee they fit your vehicle.", Category=_categoryRepository.GetAllCategories.ToList()[0], ImageUrl="https://www.rockauto.com/info/568/JBR962EVC__ra_p.jpg", IsInStock=true,IsOnSale=false,ImageThumbnailUrl="https://www.rockauto.com/info/568/JBR962EVC__ra_t.jpg"}
+         new Part {PartId=2, Name="MELLING 31035S Timing Chain Set", Price=124.89M, Description="Melling is a leader in engineering design, development, prototype, testing and assembly of fluid delivery products and related items for the automotive OEM industry as well as the aftermarket industry. In addition to being a leading manufacturer of aftermarket oil pumps, they also offer camshafts, timing components, valve train, cylinder sleeves, expansion plugs, and tools for the engine building industry.", Category=_categoryRepository.GetAllCategories.ToList()[0], ImageUrl="https://www.rockauto.com/info/583/3-1035S-1-KIT__ra_p.jpg", IsInStock=true,IsOnSale=false,ImageThumbnailUrl="https://www.rockauto.com/info/583/3-1035S-1-KIT__ra_t.jpg"}
+         new Part {PartId=3, Name="KYB SR4241 Strut-Plus", Price=121.79M, Description="Includes a KYB Excel-G strut. Excel-G struts use OE quality components to restore the vehicle's original designed performance. KYB tests each spring design and does not consolidate applications, to make sure that every Strut-Plus restores vehicle ride height to within the manufacturer's specifications. Damping valves are assembled by hand to restore the handling and stability to what the vehicle manufacturer intended. This is critical to make sure electronic safety systems, like Electronic Stability Control and Automated Braking, work as designed. The strongest and thickest strut bodies and brackets in the industry minimize flex, to keep the alignment correct and you in control. OE or better strut mounts prevent squeaking and rattling. Each part is tested for durability, and each design is fit checked and ride tested, for ease of installation and performance.", Category=_categoryRepository.GetAllCategories.ToList()[0], ImageUrl="https://www.rockauto.com/info/63/SR4241__ra_p.jpg", IsInStock=true,IsOnSale=false,ImageThumbnailUrl="https://www.rockauto.com/info/63/SR4241__ra_t.jpg"}
+      };
+
+      public IEnumerable<Part> GetPartsOnSale => throw new NotImplementedException();
+
+      public Part GetPartById(int partId)
+      {
+         return GetAllParts.FirstOrDefault(p => p.PartId == partId);
+      }
+   }
+}
