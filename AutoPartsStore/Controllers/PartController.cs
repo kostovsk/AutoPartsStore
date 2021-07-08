55aa707028df27e7a154a0b69a992642b99bb7b5
Stoycho Kostov <stoichokostov@gmail.com>
2021-07-07 20:42:42 -0500

    adding controllers, injecting services and adding action methods

---
+++ AutoPartsStore/Controllers/PartController.cs
@@ -0,0 +1,26 @@
+﻿using AutoPartsStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoPartsStore.Controllers
+{
+   public class PartController : Controller
+   {
+      private readonly IPartRepository _partRepository;
+      private readonly ICategoryRepository _categoryRepository;
+
+      public PartController(IPartRepository partRepository, ICategoryRepository categoryRepository)
+      {
+         _partRepository = partRepository;
+         _categoryRepository = categoryRepository;
+      }
+
+      public ViewResult List()
+      {
+         return View(_partRepository.GetAllParts);
+      }
+   }
+}
