b672b817d6c0179fd418be3b4dd6a7b66c902d33
Stoycho Kostov <stoichokostov@gmail.com>
2021-07-09 23:50:47 -0500

    adding connection string

--- AutoPartsStore/Startup.cs
+++ AutoPartsStore/Startup.cs
@@ -2,6 +2,8 @@ using AutoPartsStore.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -13,10 +15,21 @@ namespace AutoPartsStore
 {
    public class Startup
    {
+      public IConfiguration Configuration { get; }
+
+      public Startup(IConfiguration configuration)
+      {
+         Configuration = configuration;
+      }
+
       // This method gets called by the runtime. Use this method to add services to the container.
       // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
       public void ConfigureServices(IServiceCollection services)
       {
+         services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+         );
+
          services.AddControllersWithViews();
          services.AddScoped<ICategoryRepository, CategoryRepository>();
          services.AddScoped<IPartRepository, PartRepository>();
