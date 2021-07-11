9d116b4f50a205e4062cd3a2f15c3f3dca9ae581
Stoycho Kostov <stoichokostov@gmail.com>
2021-07-11 15:44:20 -0500

    creating home controller

---
+++ AutoPartsStore/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using AutoPartsStore.Models;
+using AutoPartsStore.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoPartsStore.Controllers
+{
+   public class HomeController : Controller
+   {
+      private readonly IPartRepository _partRepository;
+
+      public HomeController(IPartRepository partRepository)
+      {
+         _partRepository = partRepository;
+      }
+
+      public IActionResult Index()
+      {
+         var homeViewModel = new HomeViewModel
+         {
+            PartsOnSale = _partRepository.GetPartsOnSale
+         };
+
+         return View(homeViewModel);
+      }
+   }
+}
---
+++ AutoPartsStore/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using AutoPartsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoPartsStore.ViewModels
+{
+   public class HomeViewModel
+   {
+      public IEnumerable<Part> PartsOnSale { get; set; }
+   }
+}
