22f83a63be48dce4508f8323ca4f454b6d86cd71
Stoycho Kostov <stoichokostov@gmail.com>
2021-07-18 17:10:38 -0500

    shopping cart controller

---
+++ AutoPartsStore/Controllers/ShoppingCartController.cs
@@ -0,0 +1,35 @@
+﻿using AutoPartsStore.Models;
+using AutoPartsStore.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoPartsStore.Controllers
+{
+   public class ShoppingCartController : Controller
+   {
+      private readonly IPartRepository _partRepository;
+      private readonly ShoppingCart _shoppingCart;
+
+      public ShoppingCartController(IPartRepository partRepository, ShoppingCart shoppingCart)
+      {
+         _partRepository = partRepository;
+         _shoppingCart = shoppingCart;
+      }
+
+      public ViewResult Index()
+      {
+         _shoppingCart.ShoppingCartItems = _shoppingCart.GetShoppingCartItems();
+
+         var shoppingCartViewModel = new ShoppingCartViewModel
+         {
+            ShoppingCart = _shoppingCart,
+            ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+         };
+
+         return View(shoppingCartViewModel);
+      }
+   }
+}
