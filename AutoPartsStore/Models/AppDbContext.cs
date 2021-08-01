ac23d210f0d08ff02d7ffd58099e0cbb909b658d
Stoycho Kostov <stoichokostov@gmail.com>
2021-08-01 15:14:30 -0500

    add order and order detail model

--- AutoPartsStore/Models/AppDbContext.cs
+++ AutoPartsStore/Models/AppDbContext.cs
@@ -17,6 +17,9 @@ namespace AutoPartsStore.Models
       public DbSet<Part> Parts { get; set; }
       public DbSet<Category> Categories { get; set; }
       public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
+      public DbSet<Order> Orders { get; set; }
+      public DbSet<OrderDetail> OrderDetails { get; set; }
+
 
       protected override void OnModelCreating(ModelBuilder modelBuilder)
       {
---
+++ AutoPartsStore/Models/OrderDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoPartsStore.Models
+{
+   public class OrderDetail
+   {
+      public int OrderDetailId { get; set; }
+      public int OrderId { get; set; }
+      public int PartId { get; set; }
+      public Part Part { get; set; }
+      public int Amount { get; set; }
+      public decimal Price { get; set; }
+      public Order Order { get; set; }
+   }
+}
